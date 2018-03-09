bbb4395dd97b7f1155c3eceed2efe1dca83bd5e5
Lapsus <lapsusantepedis@gmail.com>
2018-03-09 14:25:58 -0500

    It's a hello world, what do you expect?

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lapsus is best pony!");
+            Console.WriteLine("Seriously though, I'm programming in C# apparently!");
+        }
+    }
+}
